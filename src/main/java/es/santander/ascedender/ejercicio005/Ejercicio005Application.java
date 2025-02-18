8e2c2b74de5eb77791de19d1ca66b9adcd817559
Alejandro Caldi <alejandro.caldi@gmail.com>
2025-02-18 23:23:35 +0100

    primer commit

---
+++ src/main/java/es/santander/ascedender/ejercicio005/Ejercicio005Application.java
@@ -0,0 +1,13 @@
+package es.santander.ascedender.ejercicio005;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ejercicio005Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ejercicio005Application.class, args);
+	}
+
+}
---
+++ src/test/java/es/santander/ascedender/ejercicio005/Ejercicio005ApplicationTests.java
@@ -0,0 +1,13 @@
+package es.santander.ascedender.ejercicio005;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio005ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
